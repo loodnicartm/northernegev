7d9c17cb16c924bdaa33d4b7ecb0cc1be4336574
Tarik <medosevictarik@\outlook.com>
2022-04-02 14:35:36 +0300

    third

--- northernegev.R
+++ northernegev.R
@@ -18,17 +18,17 @@ View(checkpoints_data)
 str(checkpoints_data)
 head(checkpoints_data)[,1:56]
 
-#create the training and test data
+#create the training and test data, 70:30
 set.seed(100)
 trainRowNumbers<-createDataPartition(checkpoints_data$NDVI_summ_slope, p=0.7, list=FALSE)
 trainData <- checkpoints_data[trainRowNumbers, ]
 testData <- checkpoints_data[-trainRowNumbers, ]
-x = trainData[3:14]
+x = trainData[3:15]
 y = trainData$NDVI_summ_slope
 
 library(skimr)
 skimmed <- skim(trainData)
-skimmed [, c(3:14)]
+skimmed [, c(1:12)]
 
 #one-hot encoding
 #creating the dummy variables-converting a categorical variable to as many binary variables as here are categories.
@@ -46,13 +46,13 @@ trainData <- predict(preProcess_range_model, newdata = trainData)
 # Append the Y variable
 trainData$NDVI_summ_slope <- y
 
-apply(trainData[, 3:14], 2, FUN=function(x){c('min'=min(x), 'max'=max(x))})
+apply(trainData[, 3:15], 2, FUN=function(x){c('min'=min(x), 'max'=max(x))})
 
 
 #visual try
-featurePlot(x = trainData[, 3:14],
+featurePlot(x = trainData[1:12],
             y = trainData$NDVI_summ_slope,
-            plot = "boxplot", auto.key=list(columns=3),
+            plot = "box",
             strip = strip.custom(par.strip.text=list(cex=.7)),
             scales = list(x=list(relation="free"),
                         y=list(relation="free")))
